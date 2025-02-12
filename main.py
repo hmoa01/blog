299f5ad44f5e598f7af507e8298504615e918140
hmoa01 <hmoa01@gmail.com>
2025-02-12 09:56:22 +0100

    add bootstrap - part 2

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template
-from post import Post
+import requests
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
-# create a blog object
-blog_data = Post()
-
 
 @app.route('/')
-def home():
-    return render_template("index.html", data=blog_data.blog())
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route('/post/<int:num>')
-def get_blog(num):
-    return render_template("post.html", data=blog_data.blog(), num=num)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
