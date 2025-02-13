15958228ea753135189b62ceaec4d947d67a89c8
hmoa01 <hmoa01@gmail.com>
2025-02-13 10:06:09 +0100

    update contact form

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import smtplib
 import requests
+import os
 
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+load_dotenv()
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+OWN_EMAIL = os.getenv("OWN_EMAIL")
+OWN_PASSWORD = os.getenv("OWN_PASSWORD")
 
 @app.route('/')
 def get_all_posts():
@@ -15,10 +21,20 @@ def get_all_posts():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com:587") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -28,6 +44,5 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
